32aa04f058cca581b73360e637553f2efabf5dd5
manjit <manjitcse@gmail.com>
2022-09-02 22:04:04 +0530

    case issue

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Common from "../../component/common/common";
-import Profile from "../../component/profile/profile";
+import Profile from "../../component/profiles/profile";
 
 export default function profile() {
   return (
