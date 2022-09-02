4d31c23f0ca7b1b3516858fcfa245118b8f8b6c1
manjit <manjitcse@gmail.com>
2022-09-02 22:06:35 +0530

    trying to fix

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Common from "../../component/common/common";
 import Profile from "../../component/profiles/profile";
 
-export default function profile() {
+export default function ProfileLayout() {
   return (
     <div>
       <Common />
